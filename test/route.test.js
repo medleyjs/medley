5858dba8fe2e9e7d7af848dd6bf929366574b75c
Nathan Woltman <nwoltman@outlook.com>
2018-03-04 13:59:38 -0500

    test: Rewrite test/route.test.js

--- test/route.test.js
+++ test/route.test.js
@@ -2,169 +2,174 @@
 
 const t = require('tap')
 const test = t.test
-const sget = require('simple-get').concat
-const app = require('..')()
+const medley = require('..')
 
 test('route - get', (t) => {
-  t.plan(1)
-  try {
-    app.route({
-      method: 'GET',
-      url: '/',
-      responseSchema: {
-        200: {
-          type: 'object',
-          properties: {
-            hello: {
-              type: 'string',
-            },
+  t.plan(3)
+
+  const app = medley()
+
+  app.route({
+    method: 'GET',
+    url: '/',
+    responseSchema: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: {
+            type: 'string',
           },
         },
       },
-      handler(req, reply) {
-        reply.send({hello: 'world'})
-      },
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
+    },
+    handler(request, reply) {
+      reply.send({hello: 'world'})
+    },
+  })
+
+  app.inject('/', (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
 })
 
 test('missing schema - route', (t) => {
-  t.plan(1)
-  try {
-    app.route({
-      method: 'GET',
-      url: '/missing',
-      handler(req, reply) {
-        reply.send({hello: 'world'})
-      },
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
+  t.plan(3)
+
+  const app = medley()
+
+  app.route({
+    method: 'GET',
+    url: '/missing',
+    handler(request, reply) {
+      reply.send({hello: 'world'})
+    },
+  })
+
+  app.inject('/missing', (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
 })
 
 test('Multiple methods', (t) => {
-  t.plan(1)
-  try {
-    app.route({
-      method: ['GET', 'DELETE'],
-      url: '/multiple',
-      handler(req, reply) {
-        reply.send({hello: 'world'})
-      },
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
+  t.plan(6)
+
+  const app = medley()
+
+  app.route({
+    method: ['GET', 'DELETE'],
+    url: '/multiple',
+    handler(request, reply) {
+      reply.send({hello: 'world'})
+    },
+  })
+
+  app.inject('/multiple', (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
+
+  app.inject({
+    method: 'DELETE',
+    url: '/multiple',
+  }, (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
 })
 
 test('Add multiple methods', (t) => {
-  t.plan(1)
-  try {
-    app.get('/add-multiple', function(req, reply) {
-      reply.send({hello: 'Bob!'})
-    })
-    app.route({
-      method: ['PUT', 'DELETE'],
-      url: '/add-multiple',
-      handler(req, reply) {
-        reply.send({hello: 'world'})
-      },
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
+  t.plan(9)
+
+  const app = medley()
+
+  app.get('/add-multiple', (request, reply) => {
+    reply.send({hello: 'Bob!'})
+  })
+
+  app.route({
+    method: ['PUT', 'DELETE'],
+    url: '/add-multiple',
+    handler(request, reply) {
+      reply.send({hello: 'world'})
+    },
+  })
+
+  app.inject('/add-multiple', (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'Bob!'})
+  })
+
+  app.inject({
+    method: 'PUT',
+    url: '/add-multiple',
+  }, (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
+
+  app.inject({
+    method: 'DELETE',
+    url: '/add-multiple',
+  }, (err, response) => {
+    t.error(err)
+    t.equal(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.payload), {hello: 'world'})
+  })
 })
 
-app.listen(0, function(err) {
-  if (err) t.error(err)
-  app.server.unref()
+test('cannot add another route after server is listening', (t) => {
+  t.plan(2)
 
-  test('cannot add another route after binding', (t) => {
-    t.plan(1)
+  const app = medley()
+
+  t.tearDown(() => app.close())
+
+  app.route({
+    method: 'GET',
+    url: '/1',
+    handler(request, reply) {
+      reply.send(1)
+    },
+  })
+
+  app.listen(0, (err) => {
+    t.error(err)
+
     try {
       app.route({
         method: 'GET',
-        url: '/another-get-route',
-        handler(req, reply) {
-          reply.send({hello: 'world'})
-        },
+        url: '/another-route',
+        handler() { },
       })
       t.fail()
-    } catch (e) {
-      t.pass()
+    } catch (err) {
+      t.equal(err.message, 'Cannot add route when app is already started!')
     }
-  })
-
-  test('route - get', (t) => {
-    t.plan(3)
-    sget({
-      method: 'GET',
-      url: 'http://localhost:' + app.server.address().port,
-    }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-      t.deepEqual(JSON.parse(body), {hello: 'world'})
-    })
-  })
-
-  test('route - missing schema', (t) => {
-    t.plan(3)
-    sget({
-      method: 'GET',
-      url: 'http://localhost:' + app.server.address().port + '/missing',
-    }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-      t.deepEqual(JSON.parse(body), {hello: 'world'})
-    })
-  })
-
-  test('route - multiple methods', (t) => {
-    t.plan(6)
-    sget({
-      method: 'GET',
-      url: 'http://localhost:' + app.server.address().port + '/multiple',
-    }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-      t.deepEqual(JSON.parse(body), {hello: 'world'})
-    })
-
-    sget({
-      method: 'DELETE',
-      url: 'http://localhost:' + app.server.address().port + '/multiple',
-    }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-      t.deepEqual(JSON.parse(body), {hello: 'world'})
-    })
   })
 })
 
 test('path can be specified in place of uri', (t) => {
   t.plan(3)
 
+  const app = medley()
+
   app.route({
     method: 'GET',
     path: '/path',
-    handler(req, reply) {
+    handler(request, reply) {
       reply.send({hello: 'world'})
     },
   })
 
-  const reqOpts = {
-    method: 'GET',
-    url: '/path',
-  }
-
-  app.inject(reqOpts, (err, res) => {
+  app.inject('/path', (err, res) => {
     t.error(err)
     t.strictEqual(res.statusCode, 200)
     t.deepEqual(JSON.parse(res.payload), {hello: 'world'})
@@ -174,21 +179,23 @@ test('path can be specified in place of uri', (t) => {
 test('invalid bodyLimit option - route', (t) => {
   t.plan(2)
 
+  const app = medley()
+
   try {
     app.route({
       bodyLimit: false,
       method: 'PUT',
-      handler: () => null,
+      handler() { },
     })
     t.fail('bodyLimit must be an integer')
   } catch (err) {
-    t.ok(err)
+    t.equal(err.message, "'bodyLimit' option must be an integer > 0. Got 'false'")
   }
 
   try {
     app.post('/url', {bodyLimit: 10000.1}, () => null)
     t.fail('bodyLimit must be an integer')
   } catch (err) {
-    t.ok(err)
+    t.equal(err.message, "'bodyLimit' option must be an integer > 0. Got '10000.1'")
   }
 })
