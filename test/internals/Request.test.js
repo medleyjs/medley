0745654c88a17346323cc17126dc9bc338143e52
Nathan Woltman <nwoltman@outlook.com>
2018-03-07 02:00:04 -0500

    Request: Add `method` and `url` getters

--- test/internals/Request.test.js
+++ test/internals/Request.test.js
@@ -13,18 +13,29 @@ t.test('Request object', (t) => {
   t.equal(request.body, undefined)
 })
 
+t.test('request.method - get', (t) => {
+  const req = {method: 'GET'}
+  t.equal(new Request(req).method, 'GET')
+  t.end()
+})
+
+t.test('request.url - get', (t) => {
+  const req = {url: '/some-url'}
+  t.equal(new Request(req).url, '/some-url')
+  t.end()
+})
+
 t.test('request.query - get', (t) => {
-  const request = new Request({url: '/path?search=1'})
-
-  t.deepEqual(request.query, {search: '1'})
-
+  const req = {url: '/path?search=1'}
+  t.deepEqual(new Request(req).query, {search: '1'})
   t.end()
 })
 
 t.test('request.query - set', (t) => {
-  const request = new Request({url: '/path?search=1'})
-  request.query = 'string'
+  const req = {url: '/path?search=1'}
+  const request = new Request(req)
 
+  request.query = 'string'
   t.equal(request.query, 'string')
 
   t.end()
