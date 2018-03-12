6a6c1e757031af9e024e62b2dd1f5cf64da6b6f5
Nathan Woltman <nwoltman@outlook.com>
2018-03-12 00:58:39 -0400

    Add benchmark just for the onFinished hook

---
+++ benchmarks/onFinished.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const medley = require('..')
+const app = medley()
+
+const responseSchema = {
+  200: {
+    hello: {type: 'string'},
+  },
+}
+
+app.addHook('onFinished', response => response)
+
+app.get('/', {responseSchema}, (request, response) => {
+  response.send({hello: 'world'})
+})
+
+app.listen(3000, (err) => {
+  if (err) {
+    throw err
+  }
+
+  console.log('Server listening on port 3000') // eslint-disable-line no-console
+})
