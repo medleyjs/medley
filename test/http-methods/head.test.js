d212d3d778ee0af6b56dba5bbc986581cd8d09e3
Nathan Woltman <nwoltman@outlook.com>
2018-03-17 16:20:44 -0400

    test: Rewrite HEAD tests and make sure Content-Length is handled properly

--- test/http-methods/head.test.js
+++ test/http-methods/head.test.js
@@ -1,65 +1,87 @@
 'use strict'
 
 const t = require('tap')
-const test = t.test
 const sget = require('simple-get').concat
-const app = require('../..')()
+const medley = require('../..')
 
-const options = {
-  responseSchema: {
+t.test('shorthand - head', (t) => {
+  t.plan(11)
+
+  const app = medley()
+
+  const responseSchema = {
     200: {
       type: 'null',
     },
-  },
-}
-
-test('shorthand - head', (t) => {
-  t.plan(1)
-  try {
-    app.head('/', options, function(req, response) {
-      response.send(null)
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
   }
-})
 
-test('missing schema - head', (t) => {
-  t.plan(1)
-  try {
-    app.head('/missing', function(req, response) {
-      response.send(null)
-    })
-    t.pass()
-  } catch (e) {
-    t.fail()
-  }
-})
-
-app.listen(0, (err) => {
-  t.error(err)
-  app.server.unref()
-
-  test('shorthand - request head', (t) => {
-    t.plan(2)
-    sget({
-      method: 'HEAD',
-      url: 'http://localhost:' + app.server.address().port,
-    }, (err, response) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-    })
+  app.head('/', {responseSchema}, (req, res) => {
+    t.pass('handler called')
+    res.send(null)
   })
 
-  test('shorthand - request head missing schema', (t) => {
-    t.plan(2)
+  app.head('/missing-schema', (req, res) => {
+    t.pass('handler called')
+    res.send(null)
+  })
+
+  app.listen(0, (err) => {
+    t.error(err)
+    app.server.unref()
+
     sget({
       method: 'HEAD',
-      url: 'http://localhost:' + app.server.address().port + '/missing',
-    }, (err, response) => {
+      url: `http://localhost:${app.server.address().port}`,
+    }, (err, response, body) => {
       t.error(err)
-      t.strictEqual(response.statusCode, 200)
+      t.equal(response.statusCode, 200)
+      t.equal(response.headers['content-length'], '4')
+      t.equal(body.toString(), '')
     })
+
+    sget({
+      method: 'HEAD',
+      url: `http://localhost:${app.server.address().port}/missing-schema`,
+    }, (err, response, body) => {
+      t.error(err)
+      t.equal(response.statusCode, 200)
+      t.equal(response.headers['content-length'], '4')
+      t.equal(body.toString(), '')
+    })
+  })
+})
+
+t.test('head request without sending a body', (t) => {
+  t.plan(9)
+
+  const app = medley()
+
+  app.head('/', (req, res) => {
+    res.send()
+  })
+
+  app.head('/length-set', (req, res) => {
+    res.set('content-length', '4').send()
+  })
+
+  app.inject({
+    method: 'HEAD',
+    url: '/',
+  }, (err, res) => {
+    t.error(err)
+    t.equal(res.statusCode, 200)
+    t.equal(res.headers.hasOwnProperty('content-length'), false)
+    t.equal(res.payload, '')
+  })
+
+  app.inject({
+    method: 'HEAD',
+    url: '/length-set',
+  }, (err, res) => {
+    t.error(err)
+    t.equal(res.statusCode, 200)
+    t.equal(res.headers.hasOwnProperty('content-length'), true)
+    t.equal(res.headers['content-length'], '4')
+    t.equal(res.payload, '')
   })
 })
